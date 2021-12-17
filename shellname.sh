779bc8f73be40cf902bcd52ae9502a9c0671366b
orion3 <noreply@orion3>
2021-12-17 13:37:43 +0700

    Fix: typo

--- shellname.sh
+++ shellname.sh
@@ -1,10 +1,10 @@
-# Intentionally nor specifying the actual shell at the top,
+# Intentionally not specifying the actual shell at the top,
 # as the the very purpose of this file is to be sourced by whichever
-# shell is running and return this shell's name.
+# shell is running and return the shell's name.
 
-# This code was tested under sh, bash, zsh, tcsh and csh, under Linux and FreeBSD
-# In FreeBSD the `ps -p` returns full path to the shell executable, not just it's name,
-# so we had to adjust for that with another grep. We're also not using the -P flag with
+# This code was tested with sh, bash, zsh, tcsh and csh; On Linux and FreeBSD.
+# In FreeBSD the `ps -p` returns the full path to the shell executable, not just its name,
+# I had to adjust for that with another call to grep. I'm also not using the -P flag with
 # grep as Perl support for grep might not be available and it's not reasonable to ask
-# the user to install Perl for just this one line of code. Which is very useful indeed.
+# the user to install Perl for just this one line of code. Which is a very useful line of code.
 ps -p $$ | tail -n1 | grep -Eo '[ \/][a-zA-Z]+' | tail -n1 | grep -Eo '[a-zA-Z]+'
