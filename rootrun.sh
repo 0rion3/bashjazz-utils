a3b8bc6df7315fa98a1a39e8602acbba53204071
orion3 <noreply@orion3>
2023-02-15 19:36:36 +0700

    Fix: typo, forgot $ in front of the variable name BASHJAZZ_PATH

--- rootrun.sh
+++ rootrun.sh
@@ -24,7 +24,7 @@ rootrun() {
     if [[ -z "$sudo_password" ]]; then
       # rootrun is typically called many times, we don't want to be checking
       # whether password was already request in every script that uses it.
-      declare -g sudo_password="$($BASHJAZZZ_PATH/utils/request_password "Password for sudo")"
+      declare -g sudo_password="$($BASHJAZZ_PATH/utils/request_password "Password for sudo")"
       echo ""
     fi
   fi
